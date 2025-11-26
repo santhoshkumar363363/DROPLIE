dcbc75b0c71d14b05c8d00c82b159aa5df71f86d
GUNAAH ° <223824497+santhoshkumar363363@users.noreply.github.com>
2025-11-26 21:41:38 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,60 +1,26 @@
-// server.js
 const express = require("express");
-const http = require("http");
-const { Server } = require("socket.io");
-
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const http = require("http").createServer(app);
+const io = require("socket.io")(http, { cors: { origin: "*" } });
 
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
+app.use(express.static(__dirname));
 
-// simple password check
-app.post("/login", (req, res) => {
-  if (req.body.password === "gunapvt") {
-    const user = encodeURIComponent(req.body.username || ("User" + Math.floor(Math.random() * 10000)));
-    res.redirect("/index.html?user=" + user);
-  } else {
-    res.send("Wrong password. Go back and try again.");
-  }
+io.on("connection", socket => {
+  socket.on("join-room", room => socket.join(room));
+
+  socket.on("offer", offer =>
+    socket.to([...socket.rooms][1]).emit("offer", offer)
+  );
+  socket.on("answer", ans =>
+    socket.to([...socket.rooms][1]).emit("answer", ans)
+  );
+  socket.on("ice", ice =>
+    socket.to([...socket.rooms][1]).emit("ice", ice)
+  );
+  socket.on("message", msg =>
+    socket.to([...socket.rooms][1]).emit("message", msg)
+  );
 });
 
-io.on("connection", (socket) => {
-  console.log("connect", socket.id);
-
-  socket.on("join-room", (roomId, user) => {
-    socket.join(roomId);
-    // tell others in room that a new user joined plus the new user's socket id
-    socket.to(roomId).emit("user-joined", { id: socket.id, user });
-
-    // forward offers/answers/ice-candidates between peers by target id
-    socket.on("offer", (data) => {
-      if (data.to) io.to(data.to).emit("offer", { from: socket.id, sdp: data.sdp });
-    });
-
-    socket.on("answer", (data) => {
-      if (data.to) io.to(data.to).emit("answer", { from: socket.id, sdp: data.sdp });
-    });
-
-    socket.on("ice-candidate", (data) => {
-      if (data.to) io.to(data.to).emit("ice-candidate", { from: socket.id, candidate: data.candidate });
-    });
-
-    // chat: send to others in same room (exclude sender)
-    socket.on("chat-message", (msg) => {
-      socket.to(roomId).emit("chat-message", { from: socket.id, user: msg.user, text: msg.text });
-    });
-
-    socket.on("disconnect", () => {
-      socket.to(roomId).emit("user-left", { id: socket.id });
-    });
-  });
-
-  socket.on("disconnect", () => {
-    console.log("disconnect", socket.id);
-  });
-});
-
-const PORT = process.env.PORT || 3000;
-server.listen(PORT, () => console.log("DROPLIE listening on", PORT));
+const PORT = process.env.PORT || 10000;
+http.listen(PORT, () => console.log(`DROPLIE running on ${PORT}`));
